3f7694edf0be036eb3c0725e4fd3276d7c7bf1de
Alexander Drach <alexander-drach@mail.ru>
2020-04-24 12:28:22 +0600

    правки задач 2.2 и 2.3 ДЗ-2

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -27,28 +27,20 @@ function getAverageMark(marks){
     }else if (marks.length > 5) {
         console.log('количество оценое больше 5');
         marks = marks.slice(0, 5);
-        let sum = 0;
-        for (let i = 0; i < marks.length; i++) {
-            sum += marks[i];
-            averageMark = sum / marks.length;
-        }
-    }else {
-        let sum = 0;
-        for (let i = 0; i < marks.length; i++) {
-            sum += marks[i];
-            averageMark = sum / marks.length;
-        }
+    }
+
+    let sum = 0;
+    
+    for (let i = 0; i < marks.length; i++) {
+        sum += marks[i];
+        averageMark = sum / marks.length;
     }
     return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){    
-    // код для задачи №3 писать здесь
-    let d = new Date(dateOfBirthday);
-    let today = new Date();
-    let age = today.getFullYear() - d.getFullYear();
+function askDrink(name,dateOfBirthday){   
     let result = '';
-    if (age > 18) {
+    if (new Date().getFullYear() - dateOfBirthday.getFullYear()) {
         result = `Не желаете ли олд-фэшн, ${name}?`
     }else {
         result = `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`
