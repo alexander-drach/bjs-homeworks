e133d417769cc7c7d8f776d1471cf76f879c38c4
Alexander Drach <alexander-drach@mail.ru>
2020-05-07 12:57:26 +0600

    Решение ДЗ-5 Прототип и конструктор объекта

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,14 +1,33 @@
 function getAnimalSound(animal) {
     // код для задачи №1 писать здесь
-    // return animalSound;
+    if (animal === undefined) {
+        return null;
+    }
+    // let sound = animal.sound; можно ведь не создавать доп. переменную а сразу вернуть значение свойства объекта?
+    return animal.sound;
 }
 
 function getAverageMark(marks) {
     // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+
+    for (let i = 0; i < marks.length; i++) {
+        sum += +marks[i];
+    }
+
+    return Math.round(sum / marks.length);
 }
 
 function checkBirthday(birthday) {
+    let now = Number(new Date());
+    birthday = Number(new Date(birthday));
+    let diff = now - birthday;
+    let age = diff / 31556952000;
+    return age > 18;
     // код для задачи №3 писать здесь
     // return verdict
 }
